2d3ab24803becdcbe2f9b5bf05e6952e2f5a047f
tsukigahara <satsuki.tksm@gmail.com>
2022-11-16 21:59:06 +0100

    optimized js

--- js/myscript.js
+++ js/myscript.js
@@ -7,84 +7,47 @@ function fieldGen (){
     mainHTML.append(contentHTML);
 }
 
-
-
-
 function gamemodeGen (value){
-    var numerate = 1;
+    var result;
 
     if (value == 1){
-        var contentHTML = document.createElement("div");
-        contentHTML.classList.add("container");
-        contentHTML.classList.add("center");
-        for (var i = 0; i < 10; i++){
-            var rowsHTML = document.createElement("div");
-            rowsHTML.classList.add("rows");
-            // rowsHTML.setAttribute("id", "row-"+i);
-            contentHTML.append(rowsHTML);
-            for (let index = 0; index < 10 ; index++ ){
-                let blockHTML = document.createElement("div");
-                blockHTML.classList.add("blocks");
-                // blockHTML.setAttribute("id", "row-"+i+"block-"+index);
-                blockHTML.addEventListener("click",
-                function marked(){
-                    blockHTML.style.backgroundColor = "lightgreen";
-                    console.log("clicked"+blockHTML)
-                });
-                blockHTML.append(numerate);
-                numerate++;
-                rowsHTML.append(blockHTML);
-            }
-        }
+        result = layoutGen(10);
+        return result;
     }
     if (value == 2){
-        var contentHTML = document.createElement("div");
-        contentHTML.classList.add("container");
-        contentHTML.classList.add("center");
-        for (var i = 0; i < 9; i++){
-            var rowsHTML = document.createElement("div");
-            rowsHTML.classList.add("rows");
-            // rowsHTML.setAttribute("id", "row-"+i);
-            contentHTML.append(rowsHTML);
-            for (let index = 0; index < 9 ; index++ ){
-                let blockHTML = document.createElement("div");
-                blockHTML.classList.add("blocks");
-                // blockHTML.setAttribute("id", "row-"+i+"block-"+index);
-                blockHTML.addEventListener("click",
-                function marked(){
-                    blockHTML.style.backgroundColor = "lightgreen";
-                    console.log("clicked"+blockHTML)
-                });
-                blockHTML.append(numerate);
-                numerate++;
-                rowsHTML.append(blockHTML);
-            }
-        }
+        result = layoutGen(9);
+        return result;
     }
     if (value == 3){
-        var contentHTML = document.createElement("div");
-        contentHTML.classList.add("container");
-        contentHTML.classList.add("center");
-        for (var i = 0; i < 7; i++){
-            var rowsHTML = document.createElement("div");
-            rowsHTML.classList.add("rows");
-            // rowsHTML.setAttribute("id", "row-"+i);
-            contentHTML.append(rowsHTML);
-            for (let index = 0; index < 7 ; index++ ){
-                let blockHTML = document.createElement("div");
-                blockHTML.classList.add("blocks");
-                // blockHTML.setAttribute("id", "row-"+i+"block-"+index);
-                blockHTML.addEventListener("click",
-                function marked(){
-                    blockHTML.style.backgroundColor = "lightgreen";
-                    console.log("clicked"+blockHTML)
-                });
-                blockHTML.append(numerate);
-                numerate++;
-                rowsHTML.append(blockHTML);
-            }
-        }
+        result = layoutGen(7);
+        return result;
     }
     return contentHTML;
 }
 
+function layoutGen (value){
+    var numerate = 1;
+    var contentHTML = document.createElement("div");
+    contentHTML.classList.add("container");
+    contentHTML.classList.add("center");
+    for (var i = 0; i < value; i++){
+        var rowsHTML = document.createElement("div");
+        rowsHTML.classList.add("rows");
+        // rowsHTML.setAttribute("id", "row-"+i);
+        contentHTML.append(rowsHTML);
+        for (let index = 0; index < value ; index++ ){
+            let blockHTML = document.createElement("div");
+            blockHTML.classList.add("blocks");
+            // blockHTML.setAttribute("id", "row-"+i+"block-"+index);
+            blockHTML.addEventListener("click",
+            function marked(){
+                blockHTML.style.backgroundColor = "lightgreen";
+                console.log("clicked"+blockHTML)
+            });
+            blockHTML.append(numerate);
+            numerate++;
+            rowsHTML.append(blockHTML);
+        }
+    }
+    return contentHTML;
+}
